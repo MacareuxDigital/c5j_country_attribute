c046861515bfaef29b48f08e718526afb07b2721
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-05-05 04:18:13 +0900

    Increment package version

--- controller.php
+++ controller.php
@@ -10,7 +10,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'c5j_country_attribute';
 
-    protected $pkgVersion = '0.9';
+    protected $pkgVersion = '0.9.1';
 
     protected $appVersionRequired = '8.5.4';
 
