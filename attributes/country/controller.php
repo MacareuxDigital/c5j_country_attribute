e59de320f833eb64e15df6167ccc8f5478ff0f18
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-05-05 04:15:27 +0900

    Show pulldown for advanced search

--- attributes/country/controller.php
+++ attributes/country/controller.php
@@ -3,6 +3,7 @@ namespace Concrete\Package\C5jCountryAttribute\Attribute\Country;
 
 use Concrete\Core\Attribute\DefaultController;
 use Concrete\Core\Attribute\FontAwesomeIconFormatter;
+use Concrete\Core\Form\Service\Form;
 use Punic\Territory;
 
 class Controller extends DefaultController
@@ -16,6 +17,14 @@ class Controller extends DefaultController
             $value = $this->app->make('helper/text')->entities($this->getAttributeValue()->getValue());
         }
         $this->set('value', $value);
+    }
+
+    public function search()
+    {
+        /** @var Form $form */
+        $form = $this->app->make('helper/form');
+
+        echo $form->selectCountry($this->field('value'), '', ['noCountryText' => t('Select Country')]);
     }
 
     public function getIconFormatter()
